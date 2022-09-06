f47c27410346728181a4b7d1280b914ef307fa67
Paulo Grabin <paulograbin@gmail.com>
2022-09-05 22:09:59 -0300

    Expose http trace endpoint

--- src/main/java/com/greglturnquist/hackingspringboot/classic/Application.java
+++ src/main/java/com/greglturnquist/hackingspringboot/classic/Application.java
@@ -1,7 +1,10 @@
 package com.greglturnquist.hackingspringboot.classic;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +13,10 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+
+
+	@Bean
+	HttpTraceRepository traceRepository() {
+		return new InMemoryHttpTraceRepository();
+	}
 }
