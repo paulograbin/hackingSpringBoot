5f23bcc3afa69b39d4e246d7784872fb4717c3bd
Paulo Grabin <paulograbin@gmail.com>
2022-09-06 19:23:30 -0300

    Make tests pass

--- src/main/java/com/greglturnquist/hackingspringboot/classic/ApiItemController.java
+++ src/main/java/com/greglturnquist/hackingspringboot/classic/ApiItemController.java
@@ -1,49 +1,48 @@
 package com.greglturnquist.hackingspringboot.classic;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
+import java.net.URI;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
 
 @RestController
 public class ApiItemController {
 
     private final ItemRepository repository;
 
-    private final InventoryService service;
-
-    public ApiItemController(ItemRepository repository, InventoryService service) {
+    public ApiItemController(ItemRepository repository) {
         this.repository = repository;
-        this.service = service;
     }
 
-    @GetMapping("/api/carts")
-    Iterable<Cart> findAll() {
-        Iterable<Cart> carts = this.service.getAllCarts();
-
-        if (carts.iterator().hasNext()) {
-            return carts;
-        } else {
-            return Arrays.asList(this.service.newCart());
-        }
+    @GetMapping("/api/items")
+    Iterable<Item> findAll() {
+        return this.repository.findAll();
     }
 
-    @GetMapping("/api/carts/{id}")
-    Cart findOne(@PathVariable String id) {
-        return this.service.getCart(id) //
-                .orElseThrow(() -> new IllegalStateException("Could find cart " + id));
+    @GetMapping("/api/items/{id}")
+    Optional<Item> findOne(@PathVariable Integer id) {
+        return this.repository.findById(id);
     }
 
-    @PostMapping("/api/carts/{cartId}/add/{itemId}")
-    Cart addToCart(@PathVariable String cartId, @PathVariable Integer itemId) {
-        return this.service.addItemToCart(cartId, itemId);
+    @PostMapping("/api/items")
+    ResponseEntity<?> addNewItem(@RequestBody Item item) {
+        Item savedItem = this.repository.save(item);
+
+        return ResponseEntity.created(URI.create("/api/items/" + savedItem.getId())).body(savedItem);
     }
 
-    @DeleteMapping("/api/carts/{cartId}/remove/{itemId}")
-    Cart removeFromCart(@PathVariable String cartId, @PathVariable Integer itemId) {
-        return this.service.removeOneFromCart(cartId, itemId);
+    @PutMapping("/api/items/{id}")
+    public ResponseEntity<?> updateItem(@RequestBody Item item, @PathVariable Integer id) {
+        Item newItem = new Item(id, item.getName(), item.getDescription(), item.getPrice());
+
+        this.repository.save(newItem);
+
+        return ResponseEntity.created(URI.create("/api/items/" + id)).build();
     }
 }
